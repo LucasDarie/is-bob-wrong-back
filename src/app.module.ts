6e394ebb53cb8a90cc0460ce8c09af3bc2457c8a
Lucas Darie <lucas.darie@etu.umontpellier.fr>
2023-12-08 03:11:52 +0100

    adapt to distant db

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,13 +9,14 @@ import { AuthModule } from './auth/auth.module';
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'postgres',
-      port: 5432,
+      host: process.env.POSTGRES_HOST,
+      port: +process.env.POSTGRES_PORT,
       username: process.env.POSTGRES_USER,
       password: process.env.POSTGRES_PASSWORD,
       database: process.env.POSTGRES_DB,
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
+      ssl: process.env.NODE_ENV === 'production',
     }),
     QuestionsModule,
     AuthModule,
